dc7e895a02cce9af3c0737695fd02d0f09cd5a4c
C3pa <41503714+C3pa@users.noreply.github.com>
2025-09-22 22:39:32 +0200

    Remove runtimeData

--- MWSE/mods/NPCs Go Home/interop/init.lua
+++ MWSE/mods/NPCs Go Home/interop/init.lua
@@ -1,14 +1,3 @@
-local runtimeData = require("NPCs Go Home.components.runtimeData")
-
 local interop = {}
-
--- TODO: maybe external mods shouldn't have access to runtimeData?
-function interop.setRuntimeData(t)
-	runtimeData = t
-end
-
-function interop.getRuntimeData() return
-	runtimeData
-end
 
 return interop
